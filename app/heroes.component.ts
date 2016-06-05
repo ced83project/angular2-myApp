0010d469c1a05e749f0659d812653d955dfa1b38
Cedric <c.beyls@gmail.com>
2016-06-06 01:52:56 +0200

    Add breadcrumb

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router }            from '@angular/router-deprecated';
+import { Router, ROUTER_DIRECTIVES }            from '@angular/router-deprecated';
 
 import { Hero }                from './hero';
 import { HeroService }         from './hero.service';
@@ -9,7 +9,7 @@ import { HeroDetailComponent } from './hero-detail.component';
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
   styleUrls:  ['app/heroes.component.css'],
-  directives: [HeroDetailComponent]
+  directives: [ROUTER_DIRECTIVES, HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
