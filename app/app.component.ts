d317330ebe10fc60f780b7ea923b110a01b57e7b
Cedric <c.beyls@gmail.com>
2016-06-06 00:34:40 +0200

    Update to rc.1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from 'angular2/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
@@ -8,31 +8,7 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a [routerLink]="['Dashboard']">Dashboard</a></li>
-            <li><a [routerLink]="['Heroes']">Heroes</a></li>
-          </ul>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
-    <div class="container">
-      <h1>{{title}}</h1>
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
@@ -41,22 +17,9 @@ import { HeroService } from './hero.service';
   ]
 })
 @RouteConfig([
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-    path: '/detail/:id',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  },
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  }
+  { path: '/dashboard',  name: 'Dashboard',  component: DashboardComponent, useAsDefault: true },
+  { path: '/heroes',     name: 'Heroes',     component: HeroesComponent },
+  { path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent }
 ])
 export class AppComponent {
   title = 'Tour of Heroes';
